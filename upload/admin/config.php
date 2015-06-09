b0264245132a9144f19f5f5339fecf8b1f4146a5
Van Dao <jacknguyenvan@gmail.com>
2015-06-09 16:01:21 +0700

    server

--- upload/admin/config.php
+++ upload/admin/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://dev.muabanxeoto24h.com/admin/');
-define('HTTP_CATALOG', 'http://dev.muabanxeoto24h.com/');
+define('HTTP_SERVER', 'http://muabanxeoto24h.com/admin/');
+define('HTTP_CATALOG', 'http://muabanxeoto24h.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://dev.muabanxeoto24h.com/admin/');
-define('HTTPS_CATALOG', 'http://dev.muabanxeoto24h.com/');
+define('HTTPS_SERVER', 'http://muabanxeoto24h.com/admin/');
+define('HTTPS_CATALOG', 'http://muabanxeoto24h.com/');
 
 // DIR
-define('DIR_APPLICATION', 'D:\muabanxeoto24h\upload/admin/');
-define('DIR_SYSTEM', 'D:\muabanxeoto24h\upload/system/');
-define('DIR_DATABASE', 'D:\muabanxeoto24h\upload/system/database/');
-define('DIR_LANGUAGE', 'D:\muabanxeoto24h\upload/admin/language/');
-define('DIR_TEMPLATE', 'D:\muabanxeoto24h\upload/admin/view/template/');
-define('DIR_CONFIG', 'D:\muabanxeoto24h\upload/system/config/');
-define('DIR_IMAGE', 'D:\muabanxeoto24h\upload/image/');
-define('DIR_CACHE', 'D:\muabanxeoto24h\upload/system/cache/');
-define('DIR_DOWNLOAD', 'D:\muabanxeoto24h\upload/download/');
-define('DIR_LOGS', 'D:\muabanxeoto24h\upload/system/logs/');
-define('DIR_CATALOG', 'D:\muabanxeoto24h\upload/catalog/');
+define('DIR_APPLICATION', '/home/vandao/web/muabanxeoto24h/upload/admin/');
+define('DIR_SYSTEM', '/home/vandao/web/muabanxeoto24h/upload/system/');
+define('DIR_DATABASE', '/home/vandao/web/muabanxeoto24h/upload/system/database/');
+define('DIR_LANGUAGE', '/home/vandao/web/muabanxeoto24h/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/vandao/web/muabanxeoto24h/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/vandao/web/muabanxeoto24h/upload/system/config/');
+define('DIR_IMAGE', '/home/vandao/web/muabanxeoto24h/upload/image/');
+define('DIR_CACHE', '/home/vandao/web/muabanxeoto24h/upload/system/cache/');
+define('DIR_DOWNLOAD', '/home/vandao/web/muabanxeoto24h/upload/download/');
+define('DIR_LOGS', '/home/vandao/web/muabanxeoto24h/upload/system/logs/');
+define('DIR_CATALOG', '/home/vandao/web/muabanxeoto24h/upload/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'vandao0');
 define('DB_DATABASE', 'muabanxeoto24h');
 define('DB_PREFIX', 'oto_');
 ?>
